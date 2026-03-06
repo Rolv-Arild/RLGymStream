c09f6043d2aea68f20de2032a24d0bb8ba7a927c
Rolv-Arild <rolv_arild@hotmail.com>
2026-03-06 15:32:59 +0100

    Use Epic launcher Fix path to bots

--- src/rlgymstream/match/launcher.py
+++ src/rlgymstream/match/launcher.py
@@ -188,6 +188,7 @@ class MatchLauncher:
         game_map_upk = GAME_MAP_TO_UPK.get(setup.map_name, setup.map_name)
 
         return flat.MatchConfiguration(
+            launcher=flat.Launcher.Epic,
             player_configurations=players,
             game_map_upk=game_map_upk,
             game_mode=flat.GameMode.Soccar,
